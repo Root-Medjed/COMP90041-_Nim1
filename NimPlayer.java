aa5a45b4eb78a0d2b3544562d364222097894950
Medjed42 <65900036+Root-Medjed@users.noreply.github.com>
2020-09-14 16:07:41 +1000

    Update 1
    NimPlayer update

--- NimPlayer.java
+++ NimPlayer.java
@@ -1,10 +1,53 @@
 public class NimPlayer
 {
-    /**
-     * Empty Constructor
-     */
-    public NimPlayer()
-    {
-        
+ 
+    private String name;
+    private int stones; //the number of stones the player wants to remove in his/her turn.
+    private int wins = 0; //how many wins;
+    private int gamesCount = 0; //how many games played;
+
+    public NimPlayer(String name, int stones, int wins, int gamesCount){ //constructor & other
+        this.name = name;
+        this.stones = stones;
+        this.wins = wins;
+        this.gamesCount = gamesCount;
+
     }
+
+    public NimPlayer(){
+
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setStones(int stones){
+        this.stones = stones;  //num came from the input
+    }
+
+    public int removeStone(){ //get stone
+        return stones;
+    }
+
+    public void setNumOfWins(int wins){ //set the number of wins
+        this.wins = wins;
+    }
+    
+    public int getWins(){
+        return wins;
+    }
+
+    public void setNumOfGames(int gamesCount){ //static method to count the number of games =
+        this.gamesCount = gamesCount;
+    }
+    
+    public int getGamesCount(){
+        return gamesCount;
+    }
+
 }
