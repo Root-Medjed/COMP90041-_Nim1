ac2d8945bcbc5464caa5578f85c8b61f383d9dea
Medjed42 <65900036+Root-Medjed@users.noreply.github.com>
2020-09-15 14:15:49 +1000

    test 3

--- NimPlayer.java
+++ NimPlayer.java
@@ -1,21 +1,22 @@
 public class NimPlayer {
 
-    private String name;
-    private int stones; //the number of stones the player wants to remove in his/her turn.
-    private int wins = 0; //how many wins;
-    private int gamesCount = 0; //how many games played;
-    //private int totalRound = 0; // count how many rounds played in each round
-
-    public NimPlayer(String name, int wins, int gamesCount）//int totalRound){ //constructor & other
-        this.name = name;
-        //this.totalRound = totalRound;
-        this.wins = wins;
-        this.gamesCount = gamesCount;
-    }
-
     public NimPlayer(){
 
     }
+    
+    private String name;
+    private Integer stones; //the number of stones the player wants to remove in his/her turn.
+    private Integer wins = 0; //how many wins;
+    private Integer gamesCount = 0; //how many games played;
+    
+//constructor & other
+    
+    public NimPlayer(String name, int wins, int games, int stones){ 
+        this.name = name;
+        this.stones = stones;
+        this.wins = wins;
+    }
+
 
     public void setName(String name){
         this.name = name;
@@ -24,57 +25,34 @@ public class NimPlayer {
     public String getName(){
         return name;
     }
-
+    
+//num came from the input
+    
     public void setStones(int stones){
-        this.stones = stones;  //num came from the input
+        this.stones = stones;  
     }
-
-    public int removeStone(){ //get stone
+    
+//get stone
+    
+    public int removeStone(){ 
         return stones;
     }
-
-    public void setNumOfWins(int wins){ //set the number of wins
+    
+//set the number of wins
+    public void setNumOfWins(int wins){ 
         this.wins += wins;
     }
 
     public int getWins(){
         return wins;
     }
-
-    //public static void getWins(){
-    //System.out.println();
-    //}
-
-    public void setNumOfGames(int gamesCount){ //static method to count the number of games =
+    
+//count the number of games played in total (setter)
+    public void setGamesCount(int games) {
         this.gamesCount += gamesCount;
     }
 
     public int getGamesCount(){
         return gamesCount;
     }
-
-    public int getTotalRound() {
-        return totalRound;
-    }
-
-    //public void setTotalRound(int totalRound) {
-        //this.totalRound += totalRound;
-    //}
-
-    public void setNumOfWins(int wins){ //set the number of wins
-        this.wins = wins;
-    }
-    
-    public int getWins(){
-        return wins;
-    }
-
-    public void setNumOfGames(int gamesCount){ //static method to count the number of games =
-        this.gamesCount = gamesCount;
-    }
-    
-    public int getGamesCount(){
-        return gamesCount;
-    }
-
 }
