aab6dbb2243230f7ef3a824c75eb523236ff5252
Medjed42 <65900036+Root-Medjed@users.noreply.github.com>
2020-09-15 10:12:51 +1000

    test 2

--- NimPlayer.java
+++ NimPlayer.java
@@ -1,17 +1,16 @@
-public class NimPlayer
-{
- 
+public class NimPlayer {
+
     private String name;
     private int stones; //the number of stones the player wants to remove in his/her turn.
     private int wins = 0; //how many wins;
     private int gamesCount = 0; //how many games played;
+    //private int totalRound = 0; // count how many rounds played in each round
 
-    public NimPlayer(String name, int stones, int wins, int gamesCount){ //constructor & other
+    public NimPlayer(String name, int wins, int gamesCount）//int totalRound){ //constructor & other
         this.name = name;
-        this.stones = stones;
+        //this.totalRound = totalRound;
         this.wins = wins;
         this.gamesCount = gamesCount;
-
     }
 
     public NimPlayer(){
@@ -33,6 +32,34 @@ public class NimPlayer
     public int removeStone(){ //get stone
         return stones;
     }
+
+    public void setNumOfWins(int wins){ //set the number of wins
+        this.wins += wins;
+    }
+
+    public int getWins(){
+        return wins;
+    }
+
+    //public static void getWins(){
+    //System.out.println();
+    //}
+
+    public void setNumOfGames(int gamesCount){ //static method to count the number of games =
+        this.gamesCount += gamesCount;
+    }
+
+    public int getGamesCount(){
+        return gamesCount;
+    }
+
+    public int getTotalRound() {
+        return totalRound;
+    }
+
+    //public void setTotalRound(int totalRound) {
+        //this.totalRound += totalRound;
+    //}
 
     public void setNumOfWins(int wins){ //set the number of wins
         this.wins = wins;
