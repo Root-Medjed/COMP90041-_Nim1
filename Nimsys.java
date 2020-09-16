7481d27fd9ad50d9961b4b03c2ea061babcfff1b
Medjed42 <65900036+Root-Medjed@users.noreply.github.com>
2020-09-16 16:57:12 +1000

    Update16
    Major issue solved, formatted string

--- Nimsys.java
+++ Nimsys.java
@@ -13,8 +13,7 @@ public class Nimsys {
         System.out.println("\nPlease enter a command to continue\n");
 
         Nimsys gameObj = new Nimsys();
-
-        gameObj.command(); //invoke command console
+        gameObj.command(); //go to command
     }
 
 
@@ -42,7 +41,7 @@ public class Nimsys {
 
     public void startGame() {
 
-        System.out.print("\nPlease enter Player 1's name : "); //player1 to input name
+        System.out.print("Please enter Player 1's name : "); //player1 to input name
         String p1 = input.next();
         NimPlayer player_1 = new NimPlayer(p1, 0, 0, 0);
 
@@ -66,22 +65,21 @@ public class Nimsys {
             turnRound = 0;
             NimPlayer currentPlayer = player_1;   //init current player as player1
 
-            System.out.print("\n" + totalStones + " stones left :");
-            star(totalStones);
 
-            while (totalStones != 0) {    //decide turns of each player
+
+            do {    //decide turns of each player
                 if (turnRound % 2 == 0) {
                     currentPlayer = player_1;
                 } else {
                     currentPlayer = player_2;
                 }
-
+                System.out.print("\n\n" + totalStones + " stones left :");
+                star(totalStones);
                 System.out.print("\n" + currentPlayer.getName() + "'s turn. Enter stones to remove : ");
-                currentPlayer.setStones(input.nextInt());
-                //bug fixed for previous version
+                currentPlayer.setStones(input.nextInt());//bug fixed for previous version
                 int sn = currentPlayer.removeStone();
 
-                if (sn <= 0 || sn > upperBound) {
+                while (currentPlayer.removeStone() <= 0 || currentPlayer.removeStone() > upperBound) {
                     System.out.println("Upper bound limit exceed, " +
                             "upper bound maximum choice is " + upperBound);
                     System.out.print("\n" + currentPlayer.getName() +
@@ -89,18 +87,19 @@ public class Nimsys {
                     currentPlayer.setStones(input.nextInt());
                 }
 
-                if (sn > totalStones) {
+                while (currentPlayer.removeStone() > totalStones) {
                     System.out.print("Invalid attempt, only " + totalStones + " stones remaining! Try again: ");
                     currentPlayer.setStones(input.nextInt());
                 }
 
                 //minus the number of removed stone and print left stone if exist
-                totalStones = totalStones - sn;
-                System.out.print("\n" + totalStones + " stones left :");
-                star(totalStones);
+                totalStones = totalStones - currentPlayer.removeStone();
+                //System.out.print("\n" + totalStones + " stones left :");
+                //star(totalStones);
 
                 turnRound++; //count the turns
 
+            }while (totalStones > 0);
 
                 if (totalStones == 0) {
                     System.out.println("\nGame Over");  //round ends
@@ -114,25 +113,29 @@ public class Nimsys {
 
                     player_1.setGameCount(player_1.getGameCount() + 1);
                     player_2.setGameCount(player_2.getGameCount() + 1);
-                }
-
-            }
+               }
             System.out.print("\nDo you want to play again (Y/N): ");
             input.nextLine().toUpperCase();
+
             if (input.nextLine().toUpperCase().equals("Y")) {
                 isRunning = true;
             }else {
                 isRunning = false;
+                exit();
             }
 
         }while(isRunning);
+        //exit();
+        int win1 = player_1.getNumOfWins();
+        int win2 = player_2.getNumOfWins();
 
-            System.out.println(player_1.getName() + " won " + player_1.getNumOfWins() +
-                    " games out of " + player_1.getGameCount() + " played\n");
+        System.out.printf( "%s won %x game%s out of %x played",
+                player_1.getName(), player_1.getNumOfWins(),
+                win1 > 1?"s":"", player_1.getGameCount());
 
-            System.out.println(player_2.getName() + " won " + player_2.getNumOfWins() +
-                    " games out of " + player_2.getGameCount() + " played\n");
-
+        System.out.printf( "\n%s won %x game%s out of %x played",
+                player_2.getName(), player_2.getNumOfWins(),
+                win2 > 1?"s":"", player_2.getGameCount());
     }
 
     private void commandList () {
